99269d92aece23579455ec9f2cbbcfd873550c33
Ceci-Alsina <cec.alsina@gmail.com>
2024-05-22 15:40:47 -0300

    ajustes validaciones en js...

--- script/validaciones.js
+++ script/validaciones.js
@@ -1,35 +1,6 @@
-/*El evento oninvalid hereda un objeto event, el cual contiene unas propiedades, 
-      incluyendo la propiedad target (el elemento invalido) y el validationMessage que 
-      contiene un mensaje de texto de error. En el ejemplo de arriba, hemos neutralizado 
-      el mensaje de texto usando el método setCustomValidity()
-      var input = document.getElementById("nombre");
-      input.oninvalid = function(event) {
-        event.target.setCustomValidity("El nombre no puede estar vacío y sólo puede contener letras");
-      }
-      
-      var inputApellido = document.getElementById("apellido");
-
-//  mensaje  de error de validación
-inputApellido.oninvalid = function(event) {
-  event.target.setCustomValidity("El apellido no puede estar vacío y solo puede contener letras");
-};
-
-// Restablecer la validación cada vez que el usuario escribe algo
-inputApellido.oninput = function(event) {
-  event.target.setCustomValidity('');  // Restablece el mensaje 
-  if (!event.target.validity.valid) {
-    event.target.setCustomValidity("El apellido no puede estar vacío y solo puede contener letras");
-  }
-};
-      
-      const email = document.getElementById("email"); 
-      email.addEventListener("input", function (event) {
-        if (email.validity.typeMismatch) {
-          email.setCustomValidity("¡Se esperaba una dirección de correo electrónico!");
-        } else {
-          email.setCustomValidity("");
-        }
-      }); */
+/* Cada campo del formulario tiene un manejador de eventos oninput que establece un mensaje de validación personalizado
+ que se activa si el campo no cumple con la validación especificada por el atributo pattern o 
+ si el campo está vacío (atributo required en el HTML).*/
       document.addEventListener("DOMContentLoaded", function () {
         const formulario = document.getElementById('formularioContacto');
     
